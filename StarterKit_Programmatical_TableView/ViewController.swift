c6a5ee17b1efac2df3acb890643fd3213a34bfd0
Nishant Bhasin <nikieme3@gmail.com>
2020-12-31 02:55:43 -0500

    Added initial xcode project

---
+++ StarterKit_Programmatical_TableView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  StarterKit_Programmatical_TableView
+//
+//  Created by Nishant Bhasin on 2020-12-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+        
+    // Setup
+    private func setup() {
+        // Background colour
+        view.backgroundColor = .orange
+        
+        // Navigation controller title
+        self.title = "Custom Tableview"
+        
+        // Tableview setup
+    }
+}
